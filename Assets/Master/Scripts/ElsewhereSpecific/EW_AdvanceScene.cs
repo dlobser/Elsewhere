65590025a7b389c1cfebae19356b20c0cded36ca
dlobser <dal230@nyu.edu>
2017-11-06 15:51:24 -0500

    performance work

--- Assets/Master/Scripts/ElsewhereSpecific/EW_AdvanceScene.cs
+++ Assets/Master/Scripts/ElsewhereSpecific/EW_AdvanceScene.cs
@@ -21,6 +21,36 @@ public class EW_AdvanceScene : MonoBehaviour {
 		{
 			SceneManager.LoadScene (++nextScene);
 		}
-	}
+        if (Input.GetKeyUp(KeyCode.Alpha0)) {
+            SceneManager.LoadScene(0);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha1)) {
+            SceneManager.LoadScene(1);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha2)) {
+            SceneManager.LoadScene(2);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha3)) {
+            SceneManager.LoadScene(3);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha4)) {
+            SceneManager.LoadScene(4);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha5)) {
+            SceneManager.LoadScene(5);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha6)) {
+            SceneManager.LoadScene(6);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha7)) {
+            SceneManager.LoadScene(7);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha8)) {
+            SceneManager.LoadScene(8);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha9)) {
+            SceneManager.LoadScene(9);
+        }
+    }
     
 }
